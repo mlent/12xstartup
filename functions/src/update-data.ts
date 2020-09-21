7f8d245a8ac4e432cda873e3fc433bf9303b3cc8
Monica Lent <lent.monica@gmail.com>
2020-09-21 15:04:23 +0200

    First attempt at making the slackbot really work

--- functions/src/update-data.ts
+++ functions/src/update-data.ts
@@ -1,32 +1,95 @@
 import { APIGatewayProxyEvent, APIGatewayProxyCallback } from 'aws-lambda';
-// import axios from 'axios';
+import * as querystring from 'querystring';
+import { WebClient } from '@slack/web-api';
+import axios from 'axios';
+
+// TODO: Create shared type between client and functions
+
+type IAirtableParticipant = {
+  id: string;
+  fields: {
+    'Last Updated': string;
+    Name: string;
+    Image: string;
+    Twitter: string;
+    Status: 'online' | 'offline';
+    Slack: string;
+    Message: string;
+    Location: string;
+    'Current Project': string[]; // ID of Project
+  };
+  createdTime: string;
+};
+
+type ISlackWebhook = {
+  user_id: string;
+  channel_id: string;
+  text: string;
+};
 
 export const handler = async function (
   event: APIGatewayProxyEvent,
   _context: any,
   callback: APIGatewayProxyCallback
 ) {
-  if (event.httpMethod === 'POST') {
-    // const jsonBody = JSON.parse(event.body) as { message: string };
-
-    // console.log(jsonBody);
-
-    /*
-    const AIRTABLE_API_KEY = process.env.AIRTABLE_API_KEY;
+  if (event.httpMethod === 'POST' && event.body) {
     const SLACKBOT_API_KEY = process.env.SLACKBOT_API_KEY;
+    const AIRTABLE_API_KEY = process.env.AIRTABLE_API_KEY;
 
-    const participantRequest = axios
-      .patch(
+    const jsonBody = querystring.parse(event.body) as ISlackWebhook;
+    const client = new WebClient(SLACKBOT_API_KEY);
+
+    console.log(jsonBody);
+
+    const participants = await axios
+      .get<{ records: IAirtableParticipant[] }>(
         `https://api.airtable.com/v0/appqerg3j5MlRwEbk/Participants?api_key=${AIRTABLE_API_KEY}`
       )
-      .then((resp) => resp.data);
-    */
+      .then((resp) => resp.data.records);
 
-    callback(null, {
-      statusCode: 200,
-      headers: { 'Content-Type': 'application/json' },
-      body: event.body as string,
-    });
+    const record = participants.find(
+      (p) => p.fields.Slack === jsonBody.user_id
+    );
+
+    if (!record) {
+      callback(null, {
+        statusCode: 500,
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ message: 'Participant is not in Airtable' }),
+      });
+      return;
+    }
+
+    // Update status in Airtable
+    try {
+      await axios.patch(
+        `https://api.airtable.com/v0/appqerg3j5MlRwEbk/Participants?api_key=${AIRTABLE_API_KEY}`,
+        {
+          records: [
+            {
+              id: record.id,
+              fields: {
+                Message: jsonBody.text,
+              },
+            },
+          ],
+        }
+      );
+
+      await client.chat.postMessage({ channel: jsonBody.channel_id, text: '' });
+
+      callback(null, {
+        statusCode: 200,
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ message: 'OK' }),
+      });
+    } catch (err) {
+      callback(null, {
+        statusCode: 500,
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ mesage: err }),
+      });
+    }
   } else {
     callback(null, {
       statusCode: 500,
